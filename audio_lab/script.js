afcf235c1eb393d9f2d46d5898c5f5c540f2be68
CesarIgnacio <cesarignac@gmail.com>
2023-03-27 20:50:23 -0400

    audio_lab hw

---
+++ audio_lab/script.js
@@ -0,0 +1,102 @@
+const audio = document.getElementById("myAudio");
+
+const song1 = document.getElementById("song1");
+const song2 = document.getElementById("song2");
+const beat1 = document.getElementById("beat1");
+const beat2 = document.getElementById("beat2");
+const beat3 = document.getElementById("beat3");
+const beat4 = document.getElementById("beat4");
+const beat5 = document.getElementById("beat5");
+const beat6 = document.getElementById("beat6");
+const beat7 = document.getElementById("beat7");
+const beat8 = document.getElementById("beat8");
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}
+
+//
+
+function playsong1() {
+    song1.play();
+}
+
+function pausesong1() {
+    song1.pause();
+}
+
+function playsong2() {
+    song2.play();
+}
+
+function pausesong2() {
+    song2.pause();
+}
+
+function playbeat1() {
+    beat1.play();
+}
+
+function pausebeat1() {
+    beat1.pause();
+}
+
+function playbeat2() {
+    beat2.play();
+}
+
+function pausebeat2() {
+    beat2.pause();
+}
+
+function playbeat3() {
+    beat3.play();
+}
+
+function pausebeat3() {
+    beat3.pause();
+}
+
+function playbeat4() {
+    beat4.play();
+}
+
+function pausebeat4() {
+    beat4.pause();
+}
+
+function playbeat5() {
+    beat5.play();
+}
+
+function pausebeat5() {
+    beat5.pause();
+}
+
+function playbeat6() {
+    beat6.play();
+}
+
+function pausebeat6() {
+    beat6.pause();
+}
+
+function playbeat7() {
+    beat7.play();
+}
+
+function pausebeat7() {
+    beat7.pause();
+}
+
+function playbeat8() {
+    beat8.play();
+}
+
+function pausebeat8() {
+    beat8.pause();
+}
